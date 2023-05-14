6b9bd9657cb673372ecf54eebf8444560dcaccf8
HAJAR <elabdellaoui.hajar@gmail.com>
2023-05-14 21:36:09 +0000

    update shell header to add string functions

--- shell.h
+++ shell.h
@@ -5,14 +5,18 @@
 #include <unistd.h>
 #include <stdlib.h>
 
-
-
-
-
-
-
-
-
-
+char *_strdup(char *str);
+char **_strtok(char *str, const char *delim);
+int tokenCount(char *str, const char *delim);
+int isDelim(char c, const char *delim);
+int _strlen(char *str);
+int _strcmp(char *str1, const char *str2);
+char *_strcat(char *str1, char *str2);
+char *_strcpy(char *dest, char *src);
+int reallocate(char **lineptr, ssize_t n);
+void *_realloc(void *ptr, size_t old_size, size_t new_size);
+char *_memcpy(char *dest, char *src, size_t n);
+char *_strncat(char *dest, char *src, size_t n);
+char *_strchr(char *str, char c);
 
 #endif /* SHELL_H */
